e17f591e0af2d983b03f354ca35471f3b3958401
evarist1 <evaristchibueze@gmail.com>
2023-11-29 04:57:08 -0800

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for tens_digit in range(9):
+    for ones_digit in range(tens_digit + 1, 10):
+        print("{:02d}".format(tens_digit * 10 + ones_digit), end=', ' if ones_digit < 9 else '\n')
