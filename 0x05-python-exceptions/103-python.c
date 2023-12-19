9dd26c2d7098fefe77fc87e86ac3fbd04a861994
evarist1 <evaristchibueze@gmail.com>
2023-12-19 14:10:17 -0800

    103-python

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,85 +1,76 @@
-/*
-
- * File: 103-python.c
-
- * Auth: Type Your Name Here
-
- */
-
-
 #include <Python.h>
 
+void print_python_list(PyObject *p) {
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "[*] Invalid List Object\n");
+        return;
+    }
 
-void print_python_list(PyObject *p);
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = ((PyListObject *)p)->allocated;
 
-void print_python_bytes(PyObject *p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", allocated);
 
-void print_python_float(PyObject *p);
+    for (Py_ssize_t i = 0; i < size; ++i) {
+        PyObject *item = PyList_GetItem(p, i);
+        printf("Element %ld: ", i);
 
-
-/**
-
- * print_python_list - Prints basic info about Python lists.
-
- * @p: A PyObject list object.
-
- */
-
-void print_python_list(PyObject *p)
-
-{
-
-        Py_ssize_t size, alloc, i;
-
-        const char *type;
-
-        PyListObject *list = (PyListObject *)p;
-
-        PyVarObject *var = (PyVarObject *)p;
-
-
-        size = var->ob_size;
-
-        alloc = list->allocated;
-
-
-        fflush(stdout);
-
-
-        printf("[*] Python list info\n");
-
-        if (strcmp(p->ob_type->tp_name, "list") != 0)
-
-        {
-
-                printf("  [ERROR] Invalid List Object\n");
-
-                return;
-
+        if (PyBytes_Check(item)) {
+            printf("bytes\n");
+            printf("[.] bytes object info\n");
+            printf("  size: %ld\n", PyBytes_Size(item));
+            printf("  trying string: %s\n", PyBytes_AsString(item));
+            printf("  first 10 bytes: ");
+            for (Py_ssize_t j = 0; j < 10 && j < PyBytes_Size(item); ++j) {
+                printf("%02x ", (unsigned char)PyBytes_AsString(item)[j]);
+            }
+            printf("\n");
+        } else if (PyFloat_Check(item)) {
+            printf("float\n");
+            printf("[.] float object info\n");
+            printf("  value: %f\n", PyFloat_AsDouble(item));
+        } else if (PyList_Check(item)) {
+            printf("list\n");
+            print_python_list(item);
+        } else if (PyTuple_Check(item)) {
+            printf("tuple\n");
+            print_python_list(item);
+        } else if (PyLong_Check(item)) {
+            printf("int\n");
+            printf("[.] int object info\n");
+            printf("  value: %ld\n", PyLong_AsLong(item));
+        } else {
+            printf("%s\n", Py_TYPE(item)->tp_name);
         }
+    }
+}
 
+void print_python_bytes(PyObject *p) {
+    if (!PyBytes_Check(p)) {
+        fprintf(stderr, "[.] bytes object info\n");
+        fprintf(stderr, "  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
 
-        printf("[*] Size of the Python List = %ld\n", size);
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", PyBytes_Size(p));
+    printf("  trying string: %s\n", PyBytes_AsString(p));
+    printf("  first 10 bytes: ");
+    for (Py_ssize_t i = 0; i < 10 && i < PyBytes_Size(p); ++i) {
+        printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
+    }
+    printf("\n");
+}
 
-        printf("[*] Allocated = %ld\n", alloc);
+void print_python_float(PyObject *p) {
+    if (!PyFloat_Check(p)) {
+        fprintf(stderr, "[.] float object info\n");
+        fprintf(stderr, "  [ERROR] Invalid Float Object\n");
+        return;
+    }
 
-
-        for (i = 0; i < size; i++)
-
-        {
-
-                type = list->ob_item[i]->ob_type->tp_name;
-
-                printf("Element %ld: %s\n", i, type);
-
-                if (strcmp(type, "bytes") == 0)
-
-                        print_python_bytes(list->ob_item[i]);
-
-                else if (strcmp(type, "float") == 0)
-
-                        print_python_float(list->ob_item[i]);
-
-        }
-
+    printf("[.] float object info\n");
+    printf("  value: %f\n", PyFloat_AsDouble(p));
 }
