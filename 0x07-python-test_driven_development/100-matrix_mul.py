7a5b7df44d3e4d864615c9084f05a8e8bd25cd65
evarist1 <evaristchibueze@gmail.com>
2024-01-09 03:22:48 -0800

    correction

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -12,62 +12,32 @@ def matrix_mul(m_a, m_b):
     Raises:
         TypeError: If m_a or m_b are not lists.
         TypeError: If m_a or m_b are not lists of lists.
-        ValueError: If m_a or m_b are empty lists/matrices.
+        ValueError: If m_a or m_b are empty matrices.
         TypeError: If m_a or m_b contain a non int/float.
         TypeError: If m_a or m_b are not rectangular.
         ValueError: If m_a or m_b can't be multiplied.
     """
 
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-    m_a_empty = False
-    m_b_empty = False
-    m_a_notrect = False
-    m_b_notrect = False
-    m_a_notnum = False
-    m_b_notnum = False
-    for row in m_a:
-        if not isinstance(row, list):
-            raise TypeError("m_a must be a list of lists")
-        if len(row) != len(m_a[0]):
-            m_a_notrect = True
-        for num in row:
-            if not isinstance(num, (int, float)):
-                m_a_notnum = True
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("Both matrices must be lists")
 
-    for row in m_b:
-        if not isinstance(row, list):
-            raise TypeError("m_b must be a list of lists")
-        if len(row) != len(m_b[0]):
-            m_b_notrect = True
-        for num in row:
-            if not isinstance(num, (int, float)):
-                m_b_notnum = True
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("Both matrices must be lists of lists")
 
-    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
-        raise ValueError("m_a can't be empty")
+    if any(len(row) == 0 for row in m_a) or any(len(row) == 0 for row in m_b):
+        raise ValueError("Matrices can't be empty")
 
-    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
-        raise ValueError("m_b can't be empty")
+    if any(not all(isinstance(num, (int, float)) for num in row) for row in m_a) or \
+       any(not all(isinstance(num, (int, float)) for num in row) for row in m_b):
+        raise TypeError("Matrices should contain only integers or floats")
 
-    if m_a_notnum:
-        raise TypeError("m_a should contain only integers or floats")
-
-    if m_b_notnum:
-        raise TypeError("m_b should contain only integers or floats")
-
-    if m_a_notrect:
-        raise TypeError("each row of m_a must be of the same size")
-
-    if m_b_notrect:
-        raise TypeError("each row of m_b must be of the same size")
+    if any(len(row) != len(m_a[0]) for row in m_a) or any(len(row) != len(m_b[0]) for row in m_b):
+        raise TypeError("Each row must be of the same size")
 
     if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+        raise ValueError("Matrices can't be multiplied")
 
-    res = [[] for i in range(len(m_a))]
+    res = [[] for _ in range(len(m_a))]
 
     for i in range(len(m_a)):
         for j in range(len(m_b[0])):
@@ -77,7 +47,6 @@ def matrix_mul(m_a, m_b):
             res[i].append(c)
 
     return res
-
 
 
 if __name__ == "__main__":
