a3caf3ba6c4d732ee4349cb852d897b7d09ff0aa
evarist1 <evaristchibueze@gmail.com>
2024-03-29 09:50:12 -0700

    correction

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,22 +5,16 @@ import sys
 import requests
 
 
-def fetch_commits(repo_name, owner_name):
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    response = requests.get(url)
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits[:10]:
-            sha = commit['sha']
-            author = commit['commit']['author']['name']
-            print(f"{sha}: {author}")
-    else:
-        print("Failed to retrieve commits.")
-
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        repo_name = sys.argv[1]
-        owner_name = sys.argv[2]
-        fetch_commits(repo_name, owner_name)
-    else:
-        print("Usage: python script.py <repository_name> <owner_name>")
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    m = requests.get(url)
+    commits = m.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
